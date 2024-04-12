469b5537146778d1dbf7493eb5b677729043ef73
kiranazka <160616395+kiranazka@users.noreply.github.com>
2024-04-12 15:33:12 -0700

    error remove

--- index.ts
+++ index.ts
@@ -1,22 +1,21 @@
 #! usr/bin/env node
 import inquirer from "inquirer";
-import { TestScheduler } from "rxjs/testing";
 console.log("\nWelcome to Azka project the CLI Number Guessing Game");
 // variable const rendomNumber math.floor
 const rendomNumber = Math.floor(Math.random() * 1 + 5);
 // user input prompt use create  variable answer 
 const answer = await inquirer.prompt([
-  {
-   // use name type or message
-    name: "userGuessNumber",
-    type: "number",
-    message:"\nEnter your Guess Number(Number Limit from 1 to 5):",
+    {
+        // use name type or message
+        name: "userGuessNumber",
+        type: "number",
+        message: "\nEnter your Guess Number(Number Limit from 1 to 5):",
     },
 ]);
 // IF/Else Statement use
-if(answer.userGuessNumber === rendomNumber){
+if (answer.userGuessNumber === rendomNumber) {
     console.log("Congratulation !  You guess a Correct Number.");
 }
-else{
+else {
     console.log("sorry !  You guess Number is inCorrect.");
 }
